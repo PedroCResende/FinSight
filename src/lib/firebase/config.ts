0a15d0f6c5c157d49e23c752dc5b3f1ab24bb698
Pedro Carvalho Resende <pedrocarvalhoresende@gmail.com>
2025-09-10 00:56:02 +0000

    O que esta faltando são essas chaves?
    // Import the functions you need

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,20 +3,17 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB...Y-U",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "1:your-sender-id:web:your-app-id",
-  measurementId: "G-your-measurement-id"
+  apiKey: "AIzaSyATjUtFWIOprS9ntjc9RGAsoCjdf5409U8",
+  authDomain: "finsight-lxxmb.firebaseapp.com",
+  projectId: "finsight-lxxmb",
+  storageBucket: "finsight-lxxmb.appspot.com",
+  messagingSenderId: "686617891887",
+  appId: "1:686617891887:web:881751cbebeb78e950a2da"
 };
+
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
