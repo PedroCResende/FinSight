1ef1d5a25ba4acd64db2cb08c37afd98e2936ffc
Pedro Carvalho Resende <pedrocarvalhoresende@gmail.com>
2025-09-16 01:41:51 +0000

    Set up Firebase Backend

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyATjUtFWIOprS9ntjc9RGAsoCjdf5409U8",
-  authDomain: "finsight-lxxmb.firebaseapp.com",
-  projectId: "finsight-lxxmb",
-  storageBucket: "finsight-lxxmb.firebasestorage.app",
-  messagingSenderId: "686617891887",
-  appId: "1:686617891887:web:881751cbebeb78e950a2da"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
